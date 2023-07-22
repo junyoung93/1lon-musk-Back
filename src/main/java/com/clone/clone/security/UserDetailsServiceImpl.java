1b05dfb4358b5ca7a1342fb2b0bc0c31c7c370cb
uijin <powerlife145@gmail.com>
2023-07-22 17:07:40 +0900

    security config 추가 및 UserDetailsImpl 추가로 @AuthenticationPrincipal 추가 완료

---
+++ src/main/java/com/clone/clone/security/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.clone.clone.security;
+
+import com.clone.clone.user.User;
+import com.clone.clone.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(()-> new UsernameNotFoundException("Not Found"+email));
+
+
+
+        return new UserDetailsImpl(user);
+    }
+}
